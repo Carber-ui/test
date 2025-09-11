90f7397e52abc8bce5548b4d9b78fee7989059f9
Carber-ui <dudghks0814@gmail.com>
2025-09-11 12:40:41 +0900

    6947

---
+++ euc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("EUC Test Program\n");
+    printf("This is a test for EUC encoding.\n");
+    printf("Let's see how it handles different characters:\n");
+    printf("あいうえお\n");
+    printf("한글 테스트\n");
+    printf("😊👍🏽\n");
+
+
+
+    
+    return 0;
+}
