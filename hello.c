a37636e2fc92c729edc50a82bf29c9089f4348a2
Carber-ui <dudghks0814@gmail.com>
2025-09-18 11:10:39 +0900

    20250918

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(void) {
+
+    int n;
+    printf("정수 : ");
+    scanf("%d", &n);
+
+    (n>0)? printf("양수입니다.\n") : printf("음수입니다.\n");
 }
