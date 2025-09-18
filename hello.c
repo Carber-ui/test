df1b4279e6b133c0e2101db80c3f1efcaf789bbb
Carber-ui <dudghks0814@gmail.com>
2025-09-18 11:32:08 +0900

    25423453252

--- hello.c
+++ hello.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+
 int main(void) {
 
     int n;
-    printf("정수 : ");
+    puts ("1부터 n까지의 합을 구합니다.");
+    printf("n의 값 : ");
     scanf("%d", &n);
 
-    (n>0)? printf("양수입니다.\n") : printf("음수입니다.\n");
+    int sum = 0; // 총합
+    int i = 1;     // 반복 변수
+
+    while (1 <= n){
+                    // i가 n 이하인 동안 반복
+        sum += i;   // sum에 i를 추가
+        i++;        // i를 1 증가
+    }
+
+    printf("1부터 %d까지의 총합은 %d입니다.\n", n, sum);
+    return 0;
 }
