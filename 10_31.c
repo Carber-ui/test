65417ff690ad540191697d15101eb05353bcf21d
Carber-ui <dudghks0814@gmail.com>
2025-10-31 10:36:36 +0900

    1031수업

---
+++ 10_31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int selection(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+    }
